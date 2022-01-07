920b99181273ebe9dc6a757fd422bf1229aa3593
Robbe Derks <robbe.derks@gmail.com>
2022-01-08 00:48:54 +0100

    make into pip package format

--- owon_psu/__init__.py
+++ owon_psu/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+__version__ = '0.0.1'
+__author__ = 'Robbe Derks'
+
 import serial
 
 class OwonPSU:
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="owon_psu",
+    version="0.0.1",
+    author="Robbe Derks",
+    author_email="robbe.derks@gmail.com",
+    description="Simple Python library for controlling Owon SPE6103 and SPE3103 power supplies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=[ 'scpi', 'owon', 'SPE6103', 'SPE3103', 'simple' ],
+    url="https://github.com/robbederks/owon-psu-control",
+    project_urls = {
+      'Source Code': 'https://github.com/robbederks/owon-psu-control',
+      'Bug Tracker': 'https://github.com/robbederks/owon-psu-control/issues'
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta"
+    ],
+    install_requires=[
+      'pyserial'
+    ]
+)
