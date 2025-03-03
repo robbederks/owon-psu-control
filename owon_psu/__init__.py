1f49a3b8ba012c3ab7a1661203330afcda031149
Dmitry Bobrik <github@bobrik.com>
2025-03-03 14:06:07 +0200

    Add support for SPMxxxx (#5)
    Tested with OWON SPM6103

--- owon_psu/__init__.py
+++ owon_psu/__init__.py
@@ -7,7 +7,7 @@ import serial
 
 class OwonPSU:
 
-  SUPPORTED_DEVICES = {"OWON,SPE", "KIPRIM,DC"}
+  SUPPORTED_DEVICES = {"OWON,SPE", "OWON,SPM", "KIPRIM,DC"}
 
   def __init__(self, port, default_timeout=0.5):
     self.ser = None
